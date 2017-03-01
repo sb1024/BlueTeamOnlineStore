3a35e78787cd6a953799a7b4356886659c831f52
Navbryce <bplunk0244@inst.hcpss.org>
2017-03-01 09:00:31 -0500

    Added changes to main Window

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,4 +1,40 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 public class MainWindow {
-	//Testtesttest
+	private boolean editor = false;
+	private XMLReaderWriter xmlReaderWriter;
+	private Store store;
+	private NavBar navBar;
+	private JFrame frame; 
+	private JPanel currentContentPane;
+	
+	MainWindow(boolean editMode){		
+		xmlReaderWriter = new XMLReaderWriter();
+		try{
+			store=xmlReaderWriter.loadStore();
+		}catch(Exception ex){
+			System.out.println("The store file could not be loaded. A default store will be created.");
+			
+			String storeName = "Generic Store";
+			ParsedImageIcon 
+		}
+		
+		editor = editMode;
+		navBar = new NavBar(this);
+		
+		HomePage homePage = new HomePage(this);
+		currentContentPane=homePage;
+		
+		frame = new JFrame("Store");
+		
+		JPanel mainPanel = new JPanel();
+		mainPanel.setPreferredSize(new Dimension(1200, 720));
+		mainPanel.add(navBar);
+		mainPanel.add(currentContentPane);
+	}
+	
 }
