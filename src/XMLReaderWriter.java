673e0451551b26a768e76388eed681e67de254f7
More-Morphine <jdipas6089@inst.hcpss.org>
2017-03-02 09:09:19 -0500

    XML Update

--- src/XMLReaderWriter.java
+++ src/XMLReaderWriter.java
@@ -1,4 +1,58 @@
 
-public class XMLReaderWriter {
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
 
+public class XMLReaderWriter {
+	
+	public void createStore(Store store){
+		try{
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.newDocument();
+			
+			//root element
+			Element rootElement = doc.createElement("Store");
+			doc.appendChild(rootElement);
+
+	         //Stores element
+	         Element supercar = doc.createElement("Stores");
+	         rootElement.appendChild(supercar);
+
+	         // setting attribute to element
+	         Attr attr = doc.createAttribute("name");
+	         attr.setValue(store.getName());
+	         supercar.setAttributeNode(attr);
+
+	         //Department element
+	         Element carname = doc.createElement("Department");
+	         Attr attrType = doc.createAttribute("name");
+	         attrType.setValue();
+	         carname.setAttributeNode(attrType);
+	         carname.appendChild(
+	         doc.createTextNode("Ferrari 101"));
+	         supercar.appendChild(carname);
+
+	         Element carname1 = doc.createElement("carname");
+	         Attr attrType1 = doc.createAttribute("type");
+	         attrType1.setValue("sports");
+	         carname1.setAttributeNode(attrType1);
+	         carname1.appendChild(
+	         doc.createTextNode("Ferrari 202"));
+	         supercar.appendChild(carname1);
+		}catch(Exception e){
+		}
+	}
+	
+	public void loadStore(){
+		
+	}
+	
+	public void saveStore(Store store){
+		
+	}
+	
+	public void addOrder(Order order){
+		
+	}
 }
