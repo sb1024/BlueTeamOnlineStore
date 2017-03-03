bfef4f034d0fd77b5f23d95ef7812982a049a04c
FaithSki <cvick3132@inst.hcpss.org>
2017-03-03 09:05:29 -0500

    finished product order, progress on shopping cart
    need to code addProductOrder with a parameter for a ProductOrder

--- src/ProductOrder.java
+++ src/ProductOrder.java
@@ -3,11 +3,28 @@ import java.util.ArrayList;
 public class ProductOrder {
 	
 	// variables
-	private ArrayList<Product> products;
-	private ArrayList<Integer> quantities;
+	private Product product;
+	private int quantity;
 	
 	// constructor
-	ProductOrder(){
-		
+	ProductOrder(Product inputProduct, int inputQuantity){
+		product = inputProduct;
+		quantity = inputQuantity;
+	}
+	
+	// get methods
+	public Product getProduct(){
+		return product;
+	}
+	public int getQuantity(){
+		return quantity;
+	}
+	
+	// set methods
+	public void setProduct(Product inputProduct){
+		product = inputProduct;
+	}
+	public void setQuantity(int inputQuantity){
+		quantity = inputQuantity;
 	}
 }
--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -5,8 +5,41 @@ public class ShoppingCart {
 	// variables
 	private ArrayList<ProductOrder> productOrders;
 	
-	// constructor
+	// constructors
 	ShoppingCart(){
-		
+		productOrders = new ArrayList<ProductOrder>();
+	}
+	ShoppingCart(ProductOrder firstProductOrder){
+		productOrders = new ArrayList<ProductOrder>();
+		productOrders.add(firstProductOrder);
+	}
+	ShoppingCart(ArrayList<ProductOrder> inputProductOrders){
+		productOrders = inputProductOrders;
+	}
+	
+	// get method
+	public ArrayList<ProductOrder> getProductOrders(){
+		return productOrders;
+	}
+	
+	// other methods
+	public void addProductOrder(Product inputProduct, int inputQuantity){
+		boolean duplicate = false;
+		int duplicateIndex = 0;
+		for(ProductOrder activeProductOrder : productOrders){
+			if(activeProductOrder.getProduct() == inputProduct){
+				duplicateIndex = productOrders.indexOf(this);
+				duplicate = true;
+			}
+		}
+		if(duplicate){
+			ProductOrder activeProductOrder = productOrders.get(duplicateIndex);
+			productOrders.remove(duplicateIndex);
+			activeProductOrder.setQuantity(activeProductOrder.getQuantity() + inputQuantity);
+			productOrders.add(duplicateIndex, activeProductOrder);
+		}
+		else{
+			productOrders.add(new ProductOrder(inputProduct, inputQuantity));
+		}
 	}
 }
